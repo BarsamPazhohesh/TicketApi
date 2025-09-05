68eeb2605eade350f8dbaea0309f024feff37de8
MohsenBg <moh.1380.1393@gmail.com>
2025-09-05 12:42:03 +0330

    feat(handler):add ticket handler read and create

--- internal/handler/app_handlers.go
+++ internal/handler/app_handlers.go
@@ -4,10 +4,12 @@ import "ticket-api/internal/repository"
 
 type AppHandlers struct {
 	Version *VersionHandler
+	Ticket  *TicketHandler
 }
 
 func NewAppHandlers(repos *repository.AppRepositories) *AppHandlers {
 	return &AppHandlers{
-		Version: &VersionHandler{Repo: repos.Version},
+		Version: NewVersionHandler(repos.Version),
+		Ticket:  NewTicketHandler(repos.Ticket),
 	}
 }
