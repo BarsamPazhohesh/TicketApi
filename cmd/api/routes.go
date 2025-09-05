f94de4ec40eed88daa7496d0a6749354530fc312
MohsenBg <moh.1380.1393@gmail.com>
2025-09-05 12:42:28 +0330

    feat(ticket):add ticket routes and mongodb database

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -13,7 +13,12 @@ func (app *application) routes() http.Handler {
 
 	v1 := g.Group("/api/v1")
 	{
+		// Version route
 		v1.GET("", app.handlers.Version.GetCurrentVersionHandler)
+
+		// Ticket routes
+		v1.POST("/tickets", app.handlers.Ticket.CreateTicketHandler) // create new ticket
+		v1.GET("/tickets/:id", app.handlers.Ticket.GetTicketHandler) // get ticket by ID
 	}
 
 	g.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
