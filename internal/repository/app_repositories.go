208672f02691e9ba4e4de730b04479a32c0ba85b
MohsenBg <moh.1380.1393@gmail.com>
2025-09-05 12:41:24 +0330

    feat(ticket-repo):add ticket repository create and read

--- internal/repository/app_repositories.go
+++ internal/repository/app_repositories.go
@@ -3,14 +3,18 @@ package repository
 import (
 	"database/sql"
 	"ticket-api/internal/db/version"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 type AppRepositories struct {
 	Version *VersionRepository
+	Ticket  *TicketRepository
 }
 
-func NewRepositories(db *sql.DB) *AppRepositories {
+func NewRepositories(sqldb *sql.DB, mongodb *mongo.Database) *AppRepositories {
 	return &AppRepositories{
-		Version: &VersionRepository{queries: version.New(db)},
+		Version: NewVersionRepository(version.New(sqldb)),
+		Ticket:  NewTicketRepository(mongodb),
 	}
 }
