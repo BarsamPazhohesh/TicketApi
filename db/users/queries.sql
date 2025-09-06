42386152d9e4c7dd9762f29fbc8e678a3a4f9445
rezishon <rezashahmoradi.81@outlook.com>
2025-09-06 17:38:46 +0330

    feat(/db/users)[queries.sql + schema.sql]: add `users` table schema + needed queries in sqlc format

---
+++ db/users/queries.sql
@@ -0,0 +1,14 @@
+-- name: GetUserByUsername :one
+SELECT id FROM users
+WHERE deleted = 0
+AND status != 0
+AND username = ?;
+
+-- name: CheckUserByID :one
+SELECT count(id) exist_of_id FROM users
+WHERE deleted = 0
+AND status != 0
+AND id = ?;
+
+-- name: CreateUser :one
+INSERT INTO users (username, department_id) VALUES (?, ?) RETURNING id;
---
+++ db/users/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT,
+    department_id INTEGER NOT NULL UNIQUE,
+    created_at TEXT NOT NULL DEFAULT (datetime('now')),
+    updated_at TEXT NOT NULL DEFAULT (datetime('now')),
+    status INT2 NOT NULL DEFAULT 1,
+    deleted INT2 NOT NULL DEFAULT 0,
+    Foreign Key (department_id) REFERENCES departments(id)
+);
