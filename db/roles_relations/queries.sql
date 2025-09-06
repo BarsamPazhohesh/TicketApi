48acb340f8afb70d28a0db5ab4ae8b60e1a929c7
rezishon <rezashahmoradi.81@outlook.com>
2025-09-06 17:41:21 +0330

    feat(/db/roles_relations)[queries.sql + schema.sql]: add `users_roles_relation` table schema + needed queries in sqlc format

--- db/roles_relations/queries.sql
+++ db/roles_relations/queries.sql
@@ -1,2 +1,5 @@
 -- name: AddApiHandlerToRolesRelation :exec
 INSERT INTO api_handlers_roles_relation (api_handler_id, role_id) VALUES (?, ?);
+
+-- name: AddUsersToRolesRelation :exec
+INSERT INTO users_roles_relation (user_id, role_id) VALUES (?, ?);
--- db/roles_relations/schema.sql
+++ db/roles_relations/schema.sql
@@ -7,3 +7,13 @@ CREATE TABLE api_handlers_roles_relation(
     FOREIGN KEY (api_handler_id) REFERENCES api_handlers(id) ON DELETE CASCADE,
     FOREIGN KEY (role_id) REFERENCES Roles(id) ON DELETE CASCADE
 );
+
+CREATE TABLE users_roles_relation (
+    user_id INTEGER NOT NULL,
+    role_id INTEGER NOT NULL,
+    status INT2 NOT NULL DEFAULT 1,
+    deleted INT2 NOT NULL DEFAULT 0,
+    PRIMARY KEY (user_id, role_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (role_id) REFERENCES Roles(id) ON DELETE CASCADE
+);
