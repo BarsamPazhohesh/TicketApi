3d92658e2b3c303c496a0f9ec756d6c3494a6a15
rezishon <rezashahmoradi.81@outlook.com>
2025-09-06 17:28:01 +0330

    feat(/cmd/migrate/migrations/)[create_users_table]: add migrations for `users` table on up & down

---
+++ cmd/mingrate/migrations/000006_create_users_table.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT,
+    department_id INTEGER NOT NULL UNIQUE,
+    created_at TEXT NOT NULL DEFAULT (datetime('now')),
+    updated_at TEXT NOT NULL DEFAULT (datetime('now')),
+    status INT2 NOT NULL DEFAULT 1,
+    deleted INT2 NOT NULL DEFAULT 0,
+    Foreign Key (department_id) REFERENCES departments(id)
+);
+
+INSERT INTO users (username, department_id) VALUES ('SampleUser', 1) RETURNING id;
