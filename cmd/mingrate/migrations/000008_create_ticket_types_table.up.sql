6bc537d6d75247e18dd70f256dbe13a0028fe0b5
rezishon <rezashahmoradi.81@outlook.com>
2025-09-06 17:28:52 +0330

    feat(/cmd/migrate/migrations/)[create_ticket_types_table]: add migrations for `ticket_types` table on up & down

---
+++ cmd/mingrate/migrations/000008_create_ticket_types_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS ticket_types(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL UNIQUE,
+    description TEXT,
+    status INT2 NOT NULL DEFAULT 1,
+    deleted INT2 NOT NULL DEFAULT 0
+);
+
+INSERT INTO ticket_types (title, description) VALUES ('SampleType', 'its a sample ticket type!') RETURNING id;
