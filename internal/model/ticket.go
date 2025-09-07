fe18df5e9736e0e885e533c34d130d8233bc9930
MohsenBg <moh.1380.1393@gmail.com>
2025-09-07 10:57:49 +0330

    feat(ticket):add ticket model

--- internal/model/ticket.go
+++ internal/model/ticket.go
@@ -2,20 +2,17 @@ package model
 
 import (
 	"time"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 // Ticket is the MongoDB model for tickets
 type Ticket struct {
-	ID          primitive.ObjectID `bson:"_id,omitempty"`
-	UserID      string             `bson:"userId"`      // شناسه کاربر
-	Type        string             `bson:"type"`        // نوع تیکت
-	Priority    string             `bson:"priority"`    // اولویت
-	Title       string             `bson:"title"`       // عنوان
-	Body        string             `bson:"body"`        // متن بدنه
-	Attachments []string           `bson:"attachments"` // پیوست آرایه آدرس فایل
-	Done        bool               `bson:"done"`        // وضعیت انجام شده
-	CreatedAt   time.Time          `bson:"createdAt"`   // زمان ایجاد
-	UpdatedAt   time.Time          `bson:"updatedAt"`   // زمان بروزرسانی
+	ID        string        `bson:"_id"`       // UUID generated in Go
+	TrackCode string        `bson:"TrackCode"` // 8-char user-facing code
+	UserID    int           `bson:"userId"`
+	Type      int           `bson:"type"`
+	Title     string        `bson:"title"`
+	Done      bool          `bson:"done"`
+	CreatedAt time.Time     `bson:"createdAt"`
+	UpdatedAt time.Time     `bson:"updatedAt"`
+	Chat      []ChatMessage `bson:"chat"`
 }
