6d7d787e50cbf6a7c823af27fbe3b1e28e3d3ef5
rezishon <rezashahmoradi.81@outlook.com>
2025-09-13 15:09:10 +0330

    feat(internal/repository)[api_handlers]: add `ApiHandler` repository
    Contains:
    - ApiHandlerRepository
    - NewApiHandlerRepository
    - AddApiHandler

---
+++ internal/repository/api_handlers.go
@@ -0,0 +1,26 @@
+package repository
+
+import (
+	"context"
+	"ticket-api/internal/db/api_handlers"
+)
+
+type ApiHandlerRepository struct {
+	queries *api_handlers.Queries
+}
+
+func NewApiHandlerRepository(queries *api_handlers.Queries) *ApiHandlerRepository {
+	return &ApiHandlerRepository{
+		queries: queries,
+	}
+}
+
+func (repo *ApiHandlerRepository) AddApiHandler(ctx context.Context, apiHandler api_handlers.AddApiHandlerParams) (int64, error) {
+	apiHandlerId, err := repo.queries.AddApiHandler(ctx, apiHandler)
+	if err != nil {
+		return -1, err
+	}
+
+	return apiHandlerId, nil
+
+}
