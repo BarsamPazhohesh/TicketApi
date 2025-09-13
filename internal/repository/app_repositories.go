9b1e80daba20a98b8978df99e637e9083c271b25
rezishon <rezashahmoradi.81@outlook.com>
2025-09-13 15:19:41 +0330

    feat(internal/repository)[app_repositories]: add all created repositories to their pip

--- internal/repository/app_repositories.go
+++ internal/repository/app_repositories.go
@@ -2,19 +2,37 @@ package repository
 
 import (
 	"database/sql"
+	"ticket-api/internal/db/api_handlers"
+	"ticket-api/internal/db/departments"
+	"ticket-api/internal/db/roles"
+	"ticket-api/internal/db/roles_relations"
+	"ticket-api/internal/db/ticket_priorities"
+	"ticket-api/internal/db/ticket_types"
 	"ticket-api/internal/db/version"
 
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 type AppRepositories struct {
-	Version *VersionRepository
-	Ticket  *TicketRepository
+	Ticket           *TicketRepository
+	Version          *VersionRepository
+	Roles            *RolesRepository
+	Departments      *DepartmentsRepository
+	TicketTypes      *TicketTypesRepository
+	TicketPriorities *TicketPrioritiesRepository
+	ApiHandlers      *ApiHandlerRepository
+	RolesRelations   *RolesRelationsRepository
 }
 
 func NewRepositories(sqldb *sql.DB, mongodb *mongo.Database) *AppRepositories {
 	return &AppRepositories{
-		Version: NewVersionRepository(version.New(sqldb)),
-		Ticket:  NewTicketRepository(mongodb),
+		Ticket:           NewTicketRepository(mongodb),
+		Version:          NewVersionRepository(version.New(sqldb)),
+		Roles:            NewRolesRepository(roles.New(sqldb)),
+		Departments:      NewDepartmentsRepository(departments.New(sqldb)),
+		TicketTypes:      NewTicketTypesRepository(ticket_types.New(sqldb)),
+		TicketPriorities: NewTicketPrioritiesRepository(ticket_priorities.New(sqldb)),
+		ApiHandlers:      NewApiHandlerRepository(api_handlers.New(sqldb)),
+		RolesRelations:   NewRolesRelationRepository(roles_relations.New(sqldb)),
 	}
 }
