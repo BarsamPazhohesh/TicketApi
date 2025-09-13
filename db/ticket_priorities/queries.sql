9173fce45dad6dd3c10359d99a331777f600cb12
MohsenBg <moh.1380.1393@gmail.com>
2025-09-13 21:45:52 +0330

    fear(priority):add GetTicketPriorityByID

--- db/ticket_priorities/queries.sql
+++ db/ticket_priorities/queries.sql
@@ -1,2 +1,5 @@
 -- name: AddTicketPriority :exec
 INSERT INTO ticket_priorities (user_id, ticket_type_id, priority) VALUES (?, ?, ?);
+
+-- name: GetTicketPriorityByID :one
+SELECT * FROM ticket_priorities WHERE   deleted =0
