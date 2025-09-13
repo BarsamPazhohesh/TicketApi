14ea5fe7dad6da623aa83523173d6583cff79e4b
MohsenBg <moh.1380.1393@gmail.com>
2025-09-13 21:52:45 +0330

    feat(chat_message_dto):sparate chat_messages_dto from ticket dto

--- internal/dto/chat_message_dto.go
+++ internal/dto/chat_message_dto.go
@@ -1,6 +1,10 @@
 package dto
 
-import "time"
+import (
+	"ticket-api/internal/model"
+	"ticket-api/internal/util"
+	"time"
+)
 
 // ChatMessageDTO represents chat messages in responses
 type ChatMessageDTO struct {
@@ -10,4 +14,26 @@ type ChatMessageDTO struct {
 	Attachments []string  `json:"attachments,omitempty"`
 	CreatedAt   time.Time `json:"createdAt"`
 	UpdatedAt   time.Time `json:"updatedAt"`
+}
+
+type ChatMessageResponseID struct {
+	ID string `json:"id"`
+}
+
+type ChatMessageCreateRequest struct {
+	SenderID    int      `json:"senderId"`
+	Message     string   `json:"message"`
+	Attachments []string `json:"attachments,omitempty"`
+}
+
+func (r *ChatMessageCreateRequest) ToModel() *model.ChatMessage {
+	now := time.Now()
+	return &model.ChatMessage{
+		ID:          util.GenerateUUID(),
+		SenderID:    r.SenderID,
+		Message:     r.Message,
+		Attachments: r.Attachments,
+		CreatedAt:   now,
+		UpdatedAt:   now,
+	}
 }
---
+++ internal/repository/chat_repository.go
@@ -0,0 +1,43 @@
+package repository
+
+import (
+	"context"
+	"errors"
+	"ticket-api/internal/apperror"
+	"ticket-api/internal/dto"
+	"ticket-api/internal/env"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+type ChatRepository struct {
+	collection *mongo.Collection
+}
+
+// NewChatRepository creates a new ChatRepository
+func NewChatRepository(db *mongo.Database) *ChatRepository {
+	if env.GetEnvInt("ENABLE_MONGO", 0) == 0 {
+		return &ChatRepository{}
+	}
+	return &ChatRepository{
+		collection: db.Collection("tickets"),
+	}
+}
+
+// CreateChatMessageForTicket adds a chat message to an existing ticket
+func (r *TicketRepository) CreateChatMessageForTicket(ctx context.Context, ticketID string, message *dto.ChatMessageCreateRequest) (*dto.ChatMessageResponseID, *apperror.APIError) {
+	model := message.ToModel()
+	update := bson.M{
+		"$push": bson.M{"chat": model},
+	}
+
+	res, err := r.collection.UpdateOne(ctx, bson.M{"_id": ticketID}, update)
+	if err != nil {
+		return nil, apperror.Respond(apperror.ErrInternalServerError, err)
+	}
+	if res.MatchedCount == 0 {
+		return nil, apperror.Respond(apperror.ErrTicketNotFound, errors.New("ticket not found"))
+	}
+	return &dto.ChatMessageResponseID{ID: model.ID}, nil
+}
