64fdbfafb7b9d23c70c2003ec0a4b9a541999a0b
rezishon <rezashahmoradi.81@outlook.com>
2025-09-14 18:18:04 +0330

    fix(db/ticket_priorities)[queries.sql]: update the select query

--- db/ticket_priorities/queries.sql
+++ db/ticket_priorities/queries.sql
@@ -2,4 +2,8 @@
 INSERT INTO ticket_priorities (user_id, ticket_type_id, priority) VALUES (?, ?, ?);
 
 -- name: GetTicketPriorityByID :one
-SELECT * FROM ticket_priorities WHERE   deleted =0
+SELECT * FROM ticket_priorities 
+WHERE deleted = 0
+AND status != 0
+AND user_id = ?
+AND ticket_type_id = ?;
