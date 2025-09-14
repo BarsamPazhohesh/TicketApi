cb9497d891bb91f2ed19ed2862cc14f2db9b90bd
MohsenBg <moh.1380.1393@gmail.com>
2025-09-14 22:26:03 +0330

    refactor(ticket_model):rename add fileds like departmentId etc..

--- internal/model/ticket.go
+++ internal/model/ticket.go
@@ -5,16 +5,15 @@ import (
 )
 
 // Ticket is the MongoDB model for tickets
-// todo: add dep id and prarity id
 type Ticket struct {
-	ID             string        `bson:"_id"`          // Unique ticket ID (UUID)
-	UserID         int           `bson:"userId"`       // ID of the user who created the ticket
-	DepartmentID   int           `bson:"departmentId"` // Department of the user
-	TypeID         int           `bson:"typeId"`       // Type/category of the ticket
-	TicketStatusID int           `bson:"statusId"`     // Current status (open, closed, etc.)
-	Title          string        `bson:"title"`        // Short descriptive title
-	TrackCode      string        `bson:"trackCode"`    // 8-char code shown to user
-	CreatedAt      time.Time     `bson:"createdAt"`    // Ticket creation timestamp
-	UpdatedAt      time.Time     `bson:"updatedAt"`    // Last update timestamp
-	Chat           []ChatMessage `bson:"chat"`         // Conversation messages for this ticket
+	ID             string        `bson:"_id"`            // Unique ticket ID (UUID)
+	UserID         int           `bson:"userId"`         // ID of the user who created the ticket
+	DepartmentID   int           `bson:"departmentId"`   // Department of the user
+	TicketTypeID   int           `bson:"ticketTypeId"`   // Type/category of the ticket
+	TicketStatusID int           `bson:"ticketStatusId"` // Current status (open, closed, etc.)
+	Title          string        `bson:"title"`          // Short descriptive title
+	TrackCode      string        `bson:"trackCode"`      // 8-char code shown to user
+	CreatedAt      time.Time     `bson:"createdAt"`      // Ticket creation timestamp
+	UpdatedAt      time.Time     `bson:"updatedAt"`      // Last update timestamp
+	Chat           []ChatMessage `bson:"chat"`           // Conversation messages for this ticket
 }
