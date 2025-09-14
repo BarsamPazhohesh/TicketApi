7f2d4c1d4d84b9ecb8ab429f3c7bdd29dcf0e998
rezishon <rezashahmoradi.81@outlook.com>
2025-09-14 18:28:46 +0330

    feat(cmd/api)[routes]: add new handler route + format the code

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,31 +1,36 @@
 package main
 
 import (
+	"net/http"
+	_ "ticket-api/docs"
+	"ticket-api/internal/util"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	"github.com/swaggo/files"
 	"github.com/swaggo/gin-swagger"
-	"net/http"
-	_ "ticket-api/docs"
 )
 
 func (app *application) routes() http.Handler {
 	g := gin.Default()
 
-	v1 := g.Group("/api/v1")
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("phoneNumber", util.ValidatePhoneNumber)
 	}
+
+	v1 := g.Group("/api/v1/")
 	{
 		// Version route
 		v1.GET("", app.handlers.Version.GetCurrentVersionHandler)
 
 		// Ticket routes
-		v1.POST("/tickets", app.handlers.Ticket.CreateTicketHandler) // create new ticket
-		v1.GET("/tickets/:id", app.handlers.Ticket.GetTicketHandler) // get ticket by ID
+		v1.POST("tickets", app.handlers.Ticket.CreateTicketHandler) // create new ticket
+		v1.GET("tickets/:id", app.handlers.Ticket.GetTicketHandler) // get ticket by ID
 
-		v1.POST("/tickets/:id/chat", app.handlers.Chat.CreateChatHandler) // create chat
+		v1.POST("tickets/:id/chat", app.handlers.Chat.CreateChatHandler) // create chat
+
+		v1.POST("auth/LoginWithNoAuth", app.handlers.User.LoginWithNoAuth)
 	}
 
 	// Redirect /swagger → /swagger/index.html
