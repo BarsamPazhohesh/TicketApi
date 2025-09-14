59d876d702890ad2fc49acad556d2be4d1344fc7
MohsenBg <moh.1380.1393@gmail.com>
2025-09-14 09:45:33 +0330

    feat(users_repository):add GetUserById and add APIError messages to repos functions

--- internal/repository/users_repository.go
+++ internal/repository/users_repository.go
@@ -2,7 +2,12 @@ package repository
 
 import (
 	"context"
+	"database/sql"
+	"errors"
+	"os/user"
+	"ticket-api/internal/apperror"
 	"ticket-api/internal/db/users"
+	"ticket-api/internal/dto"
 )
 
 type UsersRepository struct {
@@ -15,24 +20,49 @@ func NewUsersRepository(queries *users.Queries) *UsersRepository {
 	}
 }
 
-func (repo *UsersRepository) IsUserExist(ctx context.Context, userId int64) (bool, error) {
+// Check if user exists by ID
+func (repo *UsersRepository) IsUserExist(ctx context.Context, userId int64) (bool, *apperror.APIError) {
 	_, err := repo.queries.CheckUserByID(ctx, userId)
 	if err != nil {
-		return false, err
+		if errors.Is(err, sql.ErrNoRows) {
+			return false, apperror.Respond(apperror.ErrUserNotFound, err)
+		}
+		return false, apperror.Respond(apperror.ErrInternalServerError, err)
 	}
-
 	return true, nil
 }
 
-func (repo *UsersRepository) AddUser(ctx context.Context, param users.CreateUserParams) (int64, error) {
+// Add a new user
+func (repo *UsersRepository) AddUser(ctx context.Context, param users.CreateUserParams) (*dto.IDResponse, *apperror.APIError) {
 	userId, err := repo.queries.CreateUser(ctx, param)
 	if err != nil {
-		return -1, err
+		return nil, apperror.Respond(apperror.ErrInternalServerError, err)
 	}
-
-	return userId, nil
+	return &dto.IDResponse{ID: userId}, nil
 }
 
-func (repo *UsersRepository) GetUserByUsername(ctx context.Context, username string) (int64, error) {
-	return repo.queries.GetUserByUsername(ctx, username)
+// Get user by username
+func (repo *UsersRepository) GetUserByUsername(ctx context.Context, username string) (*dto.UserDTO, *apperror.APIError) {
+	user, err := repo.queries.GetUserByUsername(ctx, username)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, apperror.Respond(apperror.ErrUserNotFound, err)
+		}
+		return nil, apperror.Respond(apperror.ErrInternalServerError, err)
+	}
+
+	return dto.ToUserDTO(user), nil
+}
+
+func (repo *UsersRepository) GetUserByID(ctx context.Context, userId int) (*dto.UserDTO, *apperror.APIError) {
+	user, err := repo.queries.GetUserByID(ctx, int64(userId))
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, apperror.Respond(apperror.ErrTicketNotFound, err)
+		}
+		return nil, apperror.Respond(apperror.ErrInternalServerError, err)
+	}
+
+	userDTO := dto.ToUserDTO(user)
+	return userDTO, nil
 }
