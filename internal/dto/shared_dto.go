0a5da28d687d4d54c8c91576b5822603cb21c9cf
MohsenBg <moh.1380.1393@gmail.com>
2025-09-19 23:04:03 +0330

    feat(dto): add PagingResponse type for generic paged responses

--- internal/dto/shared_dto.go
+++ internal/dto/shared_dto.go
@@ -3,3 +3,12 @@ package dto
 type IDResponse[T int64 | int32 | string] struct {
 	ID T `json:"id"`
 }
+
+// PagingResponse is a generic paged response
+type PagingResponse[T any] struct {
+	Items      []T   `json:"items"`       // paged items
+	Total      int64 `json:"total"`       // total number of items
+	Page       int   `json:"page"`        // current page
+	PageSize   int   `json:"page_size"`   // number of items per page
+	TotalPages int   `json:"total_pages"` // total pages
+}
