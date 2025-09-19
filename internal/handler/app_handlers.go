03386b198106e40465c15f5d869732bd7eaddd08
MohsenBg <moh.1380.1393@gmail.com>
2025-09-19 23:05:51 +0330

    feat(handler): update NewAppHandlers to include services for Auth and Captcha handlers

--- internal/handler/app_handlers.go
+++ internal/handler/app_handlers.go
@@ -1,19 +1,26 @@
 package handler
 
-import "ticket-api/internal/repository"
+import (
+	"ticket-api/internal/repository"
+	"ticket-api/internal/services"
+)
 
 type AppHandlers struct {
 	Version *VersionHandler
 	Ticket  *TicketHandler
 	Chat    *ChatHandler
 	User    *UserHandler
+	Auth    *AuthHandler
+	Captcha *CaptchaHandler
 }
 
-func NewAppHandlers(repos *repository.AppRepositories) *AppHandlers {
+func NewAppHandlers(repos *repository.AppRepositories, services *services.AppServices) *AppHandlers {
 	return &AppHandlers{
 		Version: NewVersionHandler(repos.Version),
 		Ticket:  NewTicketHandler(repos.Ticket, repos.TicketTypes, repos.TicketPriorities, repos.Users, repos.Departments),
 		Chat:    NewChatHandler(repos.Ticket),
 		User:    NewUserHandler(repos.Users),
+		Auth:    NewAuthHandler(repos.Users, services.Token),
+		Captcha: NewCaptchaHandler(services.Captcha, services.Token),
 	}
 }
