1455905a935df9c27a7b8b42ee88730ebec0f459
MohsenBg <moh.1380.1393@gmail.com>
2025-09-19 22:58:37 +0330

    feat(dto): update UserDTO and add new DTOs for login and token generation

--- internal/dto/user_dto.go
+++ internal/dto/user_dto.go
@@ -6,6 +6,7 @@ type UserDTO struct {
 	ID           int64  `json:"id"`
 	Username     string `json:"username"`
 	DepartmentID int64  `json:"departmentId"`
+	Password     string `json:"-"`
 	CreatedAt    string `json:"createdAt"`
 	UpdatedAt    string `json:"updatedAt"`
 }
@@ -22,10 +23,16 @@ func (u *UserDTO) ToModel() *model.User {
 }
 
 func ToUserDTO(u model.User) *UserDTO {
+	var password string
+	if u.Password.Valid {
+		password = u.Password.String
+	}
+
 	return &UserDTO{
 		ID:           u.ID,
 		Username:     u.Username,
 		DepartmentID: u.DepartmentID,
+		Password:     password,
 		CreatedAt:    u.CreatedAt,
 		UpdatedAt:    u.UpdatedAt,
 	}
@@ -34,4 +41,28 @@ func ToUserDTO(u model.User) *UserDTO {
 type LoginWitNoAuthDTO struct {
 	Username     string `json:"username" binding:"required,phoneNumber"`
 	DepartmentID int64  `json:"departmentId" binding:"required"`
+}
+
+type LoginWithPasswordDTO struct {
+	Username string `json:"username" binding:"required,phoneNumber"`
+	Password string `json:"password" binding:"required"`
+}
+
+type SigupWithPasswordDTO struct {
+	Username     string `json:"username" binding:"required,phoneNumber"`
+	Password     string `json:"password" binding:"required"`
+	DepartmentID int64  `json:"departmentId" binding:"required"`
+}
+
+type AuthPasswordResponseDTO struct {
+	UserID      int64  `json:"userId"`
+	AccessToken string `json:"accessToken"`
+}
+
+type GenerateOneTimeTokenDTO struct {
+	Username string `json:"username" binding:"required"`
+}
+
+type OneTimeTokenResponseDTO struct {
+	Token string `json:"token"`
 }
