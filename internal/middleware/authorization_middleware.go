a8a292db3f9fef810a517a47d327baaceb4b4add
MohsenBg <moh.1380.1393@gmail.com>
2025-09-19 22:58:00 +0330

    feat(middleware): add AuthorizationMiddleware for token validation and user context setting

---
+++ internal/middleware/authorization_middleware.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	"ticket-api/internal/errx"
+
+	"github.com/gin-gonic/gin"
+	"ticket-api/internal/services/cookie"
+	"ticket-api/internal/services/token"
+)
+
+func AuthorizationMiddleware(tokenService *token.TokenService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		cookieService := cookie.NewAuthCookieService() // returns *CookieService
+		authToken, errCookie := cookieService.Get(c)
+		if errCookie != nil {
+			errApp := errx.Respond(errx.ErrUnauthorized, errCookie)
+			c.AbortWithStatusJSON(errApp.HTTPStatus, errApp)
+			return
+		}
+
+		// parse and validate the token
+		user, err := tokenService.ParseAuthToken(authToken)
+		if err != nil {
+			c.AbortWithStatusJSON(err.HTTPStatus, err)
+			return
+		}
+
+		c.Set("user", user)
+
+		c.Next()
+	}
+}
+
