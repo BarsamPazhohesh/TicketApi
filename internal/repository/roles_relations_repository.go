457a5ab2cf3942e65d5889ab8e1b22c467a0973b
rezishon <rezashahmoradi.81@outlook.com>
2025-09-24 11:41:03 +0330

    feat(/internal/repository)[roles_relations_repository.go]: update `RolesRelations` table repository
    Added
    - AddAPIKeysToRolesRelation
    - HasRouteAccess

--- internal/repository/roles_relations_repository.go
+++ internal/repository/roles_relations_repository.go
@@ -39,3 +39,49 @@ func (repo *RolesRelationsRepository) AddUsersToRolesRelation(ctx context.Contex
 	err := repo.roleRelationsQueries.AddUsersToRolesRelation(ctx, param)
 	return err
 }
+
+func (repo *RolesRelationsRepository) AddAPIKeysToRolesRelation(ctx context.Context, param roles_relations.AddAPIKeysToRolesRelationParams) error {
+	err := repo.roleRelationsQueries.AddAPIKeysToRolesRelation(ctx, param)
+	return err
+}
+
+func (repo *RolesRelationsRepository) HasRouteAccess(ctx context.Context) (bool, error) {
+	// Get ID of apiKey
+	apiKeyID, err := repo.apiKeysQueries.GetActiveAPIKeyID(ctx, "SampleKey")
+	if err != nil {
+		return false, err
+	}
+
+	// Get Roles of that ID
+	apiKeyToRolesRelationIDs, err := repo.roleRelationsQueries.GetAPIKeyRoleIDs(ctx, apiKeyID)
+	if err != nil {
+		return false, err
+	}
+
+	// Get ID of route
+	apiRouteID, err := repo.apiRoutesQueries.GetAPIRouteID(ctx, "/SampleRoute")
+	if err != nil {
+		return false, err
+	}
+
+	// Get Roles of that ID
+	apiRouteToRolesRelationIDs, err := repo.roleRelationsQueries.GetAPIRouteRoleIDs(ctx, apiRouteID)
+	if err != nil {
+		return false, err
+	}
+
+	// Compare the two IDs
+	routeRolesSet := make(map[int64]struct{}, len(apiRouteToRolesRelationIDs))
+	for _, roleID := range apiRouteToRolesRelationIDs {
+		routeRolesSet[roleID] = struct{}{}
+	}
+
+	// Check if any of the API key's roles exist in the route's roles map.
+	for _, apiKeyRoleID := range apiKeyToRolesRelationIDs {
+		if _, ok := routeRolesSet[apiKeyRoleID]; ok {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
