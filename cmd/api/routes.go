27f7523d64b3798d2bb2906d0e02be95a2a2105f
rezishon <rezashahmoradi.81@outlook.com>
2025-09-25 00:28:19 +0330

    feat(/cmd/api)[routes.go]: udpate routes
    Update routes due to use the centeralize format.

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	_ "ticket-api/docs"
 	"ticket-api/internal/middleware"
+	"ticket-api/internal/routes"
 	"ticket-api/internal/util"
 
 	"github.com/gin-gonic/gin"
@@ -27,33 +28,33 @@ func (app *application) routes() http.Handler {
 		captchaGroup := v1.Group("")
 		captchaGroup.Use(middleware.CaptchaMiddleware(app.services.Token))
 		{
-			captchaGroup.POST("auth/sigup", app.handlers.Auth.SigupWithPassword)
-			captchaGroup.GET("auth/login", app.handlers.Auth.LoginWithPassword)
-			captchaGroup.POST("tickets", app.handlers.Ticket.CreateTicketHandler)
-			captchaGroup.POST("ticket/track-code", app.handlers.Ticket.GetTicketByTrackCodeHandler)
-			captchaGroup.POST("tickets/:id/chat", app.handlers.Chat.CreateChatHandler)
+			captchaGroup.POST(routes.APIRoutes.Auth.SignUp.Path, app.handlers.Auth.SigupWithPassword)
+			captchaGroup.GET(routes.APIRoutes.Auth.Login.Path, app.handlers.Auth.LoginWithPassword)
+			captchaGroup.POST(routes.APIRoutes.Tickets.CreateTicket.Path, app.handlers.Ticket.CreateTicketHandler)
+			captchaGroup.POST(routes.APIRoutes.Tickets.GetTicketByTrackCode.Path, app.handlers.Ticket.GetTicketByTrackCodeHandler)
+			captchaGroup.POST(routes.APIRoutes.Tickets.CreateChat.Path, app.handlers.Chat.CreateChatHandler)
 		}
 
 		// Routes protected by Authorization middleware
 		authGroup := v1.Group("")
 		authGroup.Use(middleware.AuthorizationMiddleware(app.services.Token))
 		{
-			authGroup.POST("ticket/id", app.handlers.Ticket.GetTicketByIDHandler)
-			authGroup.POST("auth/LoginWithNoAuth", app.handlers.User.LoginWithNoAuth)
+			authGroup.POST(routes.APIRoutes.Tickets.GetTicketByID.Path, app.handlers.Ticket.GetTicketByIDHandler)
+			authGroup.POST(routes.APIRoutes.Auth.LoginWithNoAuth.Path, app.handlers.User.LoginWithNoAuth)
 		}
 
 		// Auth routes (no middleware for one-time token)
 		publicGroup := v1.Group("")
 		{
 			// Version and public captcha routes (no middleware)
-			publicGroup.GET("", app.handlers.Version.GetCurrentVersionHandler)
-			publicGroup.GET("captcha/new", app.handlers.Captcha.GenerateCaptchaHandler)
-			publicGroup.POST("captcha/verify", app.handlers.Captcha.VerifyCaptchaHandler)
+			publicGroup.GET(routes.APIRoutes.Versions.GetCurrentVersion.Path, app.handlers.Version.GetCurrentVersionHandler)
+			publicGroup.GET(routes.APIRoutes.Captcha.GetCaptcha.Path, app.handlers.Captcha.GenerateCaptchaHandler)
+			publicGroup.POST(routes.APIRoutes.Captcha.VerifyCaptcha.Path, app.handlers.Captcha.VerifyCaptchaHandler)
 
-			publicGroup.POST("auth/one-time-token", app.handlers.Auth.GenerateOneTimeToken)
-			publicGroup.GET("auth/login/token", app.handlers.Auth.LoginWithOneTimeToken)
+			publicGroup.POST(routes.APIRoutes.Auth.GetSingleUseToken.Path, app.handlers.Auth.GenerateOneTimeToken)
+			publicGroup.GET(routes.APIRoutes.Auth.LoginWithSingleUseToken.Path, app.handlers.Auth.LoginWithOneTimeToken)
 
-			publicGroup.POST("tickets/list", app.handlers.Ticket.ListTicketsHandler)
+			publicGroup.POST(routes.APIRoutes.Tickets.GetTicketsList.Path, app.handlers.Ticket.ListTicketsHandler)
 		}
 	}
 
