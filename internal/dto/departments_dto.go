1c6e4f6d8d065bd685cbb632d7c28eb8d4e64bf1
rezishon <rezashahmoradi.81@outlook.com>
2025-09-28 07:41:28 +0330

    feat(/internal/dto)[departments_dto.go]: update `DepartmentDTO` + update `ToModel()` of department + add `ToDepartmentDTO` function

--- internal/dto/departments_dto.go
+++ internal/dto/departments_dto.go
@@ -3,25 +3,43 @@ package dto
 import (
 	"database/sql"
 	"ticket-api/internal/db/departments"
+	"ticket-api/internal/model"
 	//"ticket-api/internal/db/departments"
 )
 
-type DepartmentDto struct {
-	ID          int64          `json:"id"`
-	Title       string         `json:"title"`
-	Description sql.NullString `json:"description"`
+type DepartmentDTO struct {
+	ID          int64   `json:"id"`
+	Title       string  `json:"title"`
+	Description *string `json:"description,omitempty"`
 }
 
-func (dt *DepartmentDto) ToModel() *departments.Department {
-	status := int64(1)
-	deleted := int64(0)
+func (dt *DepartmentDTO) ToModel() *departments.Department {
+	nullDesc := sql.NullString{}
+	if dt.Description != nil {
+		nullDesc = sql.NullString{String: *dt.Description, Valid: true}
+	} else {
+		nullDesc = sql.NullString{String: "", Valid: false}
+	}
 
 	return &departments.Department{
 		ID:          dt.ID,
 		Title:       dt.Title,
-		Description: sql.NullString{String: dt.Description.String, Valid: dt.Description.String != ""},
-		Status:      status,
-		Deleted:     deleted,
+		Description: nullDesc,
+		Status:      1,
+		Deleted:     0,
+	}
+}
+
+func ToDepartmentDTO(m model.Department) *DepartmentDTO {
+	var description *string
+	if m.Description.Valid {
+		description = &m.Description.String
+	}
+
+	return &DepartmentDTO{
+		ID:          m.ID,
+		Title:       m.Title,
+		Description: description,
 	}
 }
 
