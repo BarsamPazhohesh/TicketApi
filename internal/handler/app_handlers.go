768cb7b22fdbb55dd3d675ef22fff9d87dd2ac21
rezishon <rezashahmoradi.81@outlook.com>
2025-09-28 07:44:20 +0330

    feat(/internal/handler)[app_handlers.go]: add `Department` handler + format the code

--- internal/handler/app_handlers.go
+++ internal/handler/app_handlers.go
@@ -10,17 +10,19 @@ type AppHandlers struct {
 	Ticket  *TicketHandler
 	Chat    *ChatHandler
 	// User    *UserHandler
-	Auth    *AuthHandler
-	Captcha *CaptchaHandler
+	Auth       *AuthHandler
+	Captcha    *CaptchaHandler
+	Department *DepartmentHandler
 }
 
 func NewAppHandlers(repos *repository.AppRepositories, services *services.AppServices) *AppHandlers {
 	return &AppHandlers{
 		Version: NewVersionHandler(repos.Version),
-		Ticket:  NewTicketHandler(repos.Ticket, repos.TicketTypes, repos.TicketPriorities, repos.Users, repos.Departments),
+		Ticket:  NewTicketHandler(repos.Ticket, repos.TicketTypes, repos.TicketPriorities, repos.TicketStatus, repos.Users, repos.Departments),
 		Chat:    NewChatHandler(repos.Ticket),
 		// User:    NewUserHandler(repos.Users),
-		Auth:    NewAuthHandler(repos.Users, services.Token),
-		Captcha: NewCaptchaHandler(services.Captcha, services.Token),
+		Auth:       NewAuthHandler(repos.Users, services.Token),
+		Captcha:    NewCaptchaHandler(services.Captcha, services.Token),
+		Department: NewDepartmentHandler(repos.Departments),
 	}
 }
