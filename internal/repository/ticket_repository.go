2635febb7ea257c3004a88ed87fe5d7acbc94f47
MohsenBg <moh.1380.1393@gmail.com>
2025-10-11 13:51:13 +0330

    feat(ticket_repository):imporve validation of ticket repository

--- internal/repository/ticket_repository.go
+++ internal/repository/ticket_repository.go
@@ -48,8 +48,15 @@ func (r *TicketRepository) CreateTicket(ctx context.Context, ticketDTO *dto.Tick
 
 // GetTicketByID retrieves a single ticket by ID and converts it to TicketRaw.
 func (r *TicketRepository) GetTicketByID(ctx context.Context, id string) (*dto.TicketResponse, *errx.APIError) {
+
+	// Validate UUID
+	uid, err := uuid.Parse(id)
+	if err != nil {
+		return nil, errx.Respond(errx.ErrBadRequest, err)
+	}
+
 	var ticket model.Ticket
-	if err := r.collection.FindOne(ctx, bson.M{"_id": id}).Decode(&ticket); err != nil {
+	if err := r.collection.FindOne(ctx, bson.M{"_id": uid}).Decode(&ticket); err != nil {
 		if err == mongo.ErrNoDocuments {
 			return nil, errx.Respond(errx.ErrTicketNotFound, err)
 		}
@@ -197,11 +204,13 @@ func (r *TicketRepository) GetTickets(
 func (r *TicketRepository) SetTicketStatus(ctx context.Context, id string, statusId int64) (*dto.TicketResponse, *errx.APIError) {
 
 	// Validate UUID
-	if _, err := uuid.Parse(id); err != nil {
+	uid, err := uuid.Parse(id)
+
+	if err != nil {
 		return nil, errx.Respond(errx.ErrBadRequest, err)
 	}
 
-	filter := bson.M{"_id": id}
+	filter := bson.M{"_id": uid}
 
 	update := bson.D{
 		{Key: "$set", Value: bson.M{
@@ -216,7 +225,7 @@ func (r *TicketRepository) SetTicketStatus(ctx context.Context, id string, statu
 	opts := options.FindOneAndUpdate().SetReturnDocument(options.After).SetProjection(bson.M{"chat": 0})
 
 	var model model.Ticket
-	err := r.collection.FindOneAndUpdate(ctx, filter, update, opts).Decode(&model)
+	err = r.collection.FindOneAndUpdate(ctx, filter, update, opts).Decode(&model)
 	if err != nil {
 		return nil, errx.Respond(errx.ErrInternalServerError, err)
 	}
