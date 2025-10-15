56b2bdb2d60f15b5a9bf45b00380c2c71a5e1aee
MohsenBg <moh.1380.1393@gmail.com>
2025-10-15 09:08:41 +0330

    feat(storage)[internal/services/storage/storage.go]: implement StorageService for file management in MinIO

--- internal/services/app_services.go
+++ internal/services/app_services.go
@@ -1,18 +1,27 @@
 package services
 
 import (
+	"ticket-api/internal/services/cache"
 	"ticket-api/internal/services/captcha"
+	"ticket-api/internal/services/storage"
 	"ticket-api/internal/services/token"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/redis/go-redis/v9"
 )
 
 type AppServices struct {
-	Captcha *captcha.CaptchaService
-	Token   *token.TokenService
+	Captcha     *captcha.CaptchaService
+	Token       *token.TokenService
+	Cache       *cache.CacheService
+	FileStorage *storage.StorageService
 }
 
-func NewAppService() *AppServices {
+func NewAppService(redis *redis.Client, minio *minio.Client) *AppServices {
 	return &AppServices{
-		Captcha: captcha.NewCaptchaService(),
-		Token:   token.NewTokenService(),
+		Captcha:     captcha.NewCaptchaService(),
+		Token:       token.NewTokenService(),
+		Cache:       cache.NewCacheService(redis),
+		FileStorage: storage.NewStorageService(minio),
 	}
 }
